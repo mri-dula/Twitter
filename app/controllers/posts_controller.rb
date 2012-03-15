672b27ac63e49186145c829c9876253fd3987880
Chirantan Rajhans <chirantan.rajhans@gmail.com>
2012-03-15 18:38:37 +0100

    Refactor controller further

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all(:order => "created_at DESC")
+    @posts = current_user.timeline if logged_in?
     respond_to do |format|
       format.html
     end
